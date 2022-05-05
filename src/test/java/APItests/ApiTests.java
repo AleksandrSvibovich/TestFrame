4df235acd460f841d8103814675356dfd618ce66
alex_svibovich <asvibovich@vtb.ru>
2022-05-05 18:17:01 +0400

    updated structure of framework

--- src/test/java/APItests/ApiTests.java
+++ src/test/java/APItests/ApiTests.java
@@ -1,4 +1,18 @@
 package APItests;
 
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+import static org.hamcrest.Matchers.is;
+
 public class ApiTests {
+
+    @Test
+    public void postmanTest200(){
+        RestAssured.
+                when().get("https://postman-echo.com/get?test=123").
+                then().assertThat().statusCode(200).
+                and().body("args.test",is("123"));
+
+
+    }
 }
