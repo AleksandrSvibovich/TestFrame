46e31563edf689617b0599130b45b4606b4987e3
alex_svibovich <asvibovich@vtb.ru>
2022-05-12 17:06:45 +0400

    updated structure of framework

--- src/test/java/APItests/ApiTests.java
+++ src/test/java/APItests/ApiTests.java
@@ -2,9 +2,7 @@ package APItests;
 
 import io.qameta.allure.Description;
 import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.Header;
-import io.restassured.http.Headers;
 import io.restassured.http.Method;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
@@ -15,22 +13,21 @@ import org.testng.annotations.Test;
 
 public class ApiTests {
 
-    String apiAddress = "https://reqres.in/";
+    String apiAddress = "https://reqres.in/api";
 
-    final static int SUCCESS_CODE = 200;
-    public static final int SUCCESS_201 = 201;
-    public static final int SUCCESS_204 = 204;
-    final static int NOT_FOUND_CODE = 404;
-
+    final static int SUCCESS_200 = 200;
+    final static int SUCCESS_201 = 201;
+    final static int SUCCESS_204 = 204;
+    final static int FAIL_404 = 404;
+    final static int FAIL_400 = 400;
 
 
     @Description("Get list of users")
     @Test
     public void getListUsers() {
-        RequestSpecification requestSpec = RestAssured.given();
-        requestSpec.baseUri(apiAddress);
-        Response response = requestSpec.request(Method.GET, "api/users?page=1");
-        Assert.assertEquals(response.getStatusCode(), SUCCESS_CODE);
+        RequestSpecification requestSpec = getRequestSpecification();
+        Response response = requestSpec.request(Method.GET, "/users?page=1");
+        Assert.assertEquals(response.getStatusCode(), SUCCESS_200);
 
         JsonPath jsonPath = response.jsonPath();
         String email = jsonPath.get("data[0].email");
@@ -40,10 +37,9 @@ public class ApiTests {
     @Description("Get single user and check his or her name")
     @Test
     public void getSingleUser() {
-        RequestSpecification requestSpec = RestAssured.given();
-        requestSpec.baseUri(apiAddress);
-        Response response = requestSpec.request(Method.GET, "api/users/12");
-        Assert.assertEquals(response.getStatusCode(), SUCCESS_CODE);
+        RequestSpecification requestSpec = getRequestSpecification();
+        Response response = requestSpec.request(Method.GET, "/users/12");
+        Assert.assertEquals(response.getStatusCode(), SUCCESS_200);
 
         JsonPath jsonPath = response.jsonPath();
         String first_name = jsonPath.get("data.first_name");
@@ -53,20 +49,18 @@ public class ApiTests {
     @Description("No results of search")
     @Test
     public void checkNotFoundResult() {
-        RequestSpecification requestSpecification = RestAssured.given();
-        requestSpecification.baseUri(apiAddress);
-        Response response = requestSpecification.request(Method.GET, "api/users/1232");
-        Assert.assertEquals(response.getStatusCode(), NOT_FOUND_CODE);
+        RequestSpecification requestSpecification = getRequestSpecification();
+        Response response = requestSpecification.request(Method.GET, "/users/1232");
+        Assert.assertEquals(response.getStatusCode(), FAIL_404);
     }
 
     @Description("Get colours list endpoint")
     @Test
     public void checkColoursList() {
-        RequestSpecification specification = RestAssured.given();
-        specification.baseUri(apiAddress);
+        RequestSpecification specification = getRequestSpecification();
 
-        Response response = specification.request(Method.GET, "api/unknown");
-        Assert.assertEquals(response.getStatusCode(), SUCCESS_CODE);
+        Response response = specification.request(Method.GET, "/unknown");
+        Assert.assertEquals(response.getStatusCode(), SUCCESS_200);
 
         JsonPath path = response.jsonPath();
         Assert.assertEquals(path.get("data[1].name"), "fuchsia rose");
@@ -75,11 +69,10 @@ public class ApiTests {
     @Description("Get single color")
     @Test
     public void checkSingleColor() {
-        RequestSpecification specification = RestAssured.given();
-        specification.baseUri(apiAddress);
+        RequestSpecification specification = getRequestSpecification();
 
-        Response response = specification.request(Method.GET, "api/unknown/2");
-        Assert.assertEquals(response.getStatusCode(), SUCCESS_CODE);
+        Response response = specification.request(Method.GET, "/unknown/2");
+        Assert.assertEquals(response.getStatusCode(), SUCCESS_200);
 
         JsonPath path = response.jsonPath();
         Assert.assertTrue(path.get("data.color").equals("#C74375"));
@@ -88,145 +81,135 @@ public class ApiTests {
     @Description("No results of color search")
     @Test
     public void checkNotFoundResultColor() {
-        RequestSpecification requestSpecification = RestAssured.given();
-        requestSpecification.baseUri(apiAddress);
-        Response response = requestSpecification.request(Method.GET, "api/unknown/222");
-        Assert.assertEquals(response.getStatusCode(), NOT_FOUND_CODE);
+        RequestSpecification requestSpecification = getRequestSpecification();
+        Response response = requestSpecification.request(Method.GET, "/unknown/222");
+        Assert.assertEquals(response.getStatusCode(), FAIL_404);
     }
 
     @Description("Crate a new user")
     @Test
     public void createUser() {
-        RequestSpecification request = RestAssured.given();
-        request.baseUri(apiAddress);
+        RequestSpecification specification = getRequestSpecification();
 
         JSONObject jsonBody = new JSONObject();
         jsonBody.put("name", "Olegka");
         jsonBody.put("job", "Slesar\'");
-        request.body(jsonBody.toString());
+        specification.body(jsonBody.toString());
 
-        Response response = request.request(Method.POST, "api/users");
+        Response response = specification.request(Method.POST, "/users");
         Assert.assertEquals(response.getStatusCode(), SUCCESS_201);
     }
 
     @Description("Update user information")
     @Test
-    public void updateUserInfo(){
-        RequestSpecification request = RestAssured.given();
-        request.baseUri(apiAddress);
+    public void updateUserInfo() {
+        RequestSpecification request = getRequestSpecification();
 
         JSONObject jsonBody = new JSONObject();
         jsonBody.put("name", "Mishanja");
         jsonBody.put("job", "Boss");
 
-        request.header(new Header("Content-Type","application/json"));
+        request.header(new Header("Content-Type", "application/json"));
         request.body(jsonBody.toString());
 
-        Response response = request.request(Method.PUT,"api/users/459");
-        response.getBody().prettyPrint();
-        Assert.assertEquals(response.getStatusCode(), SUCCESS_CODE);
+        Response response = request.request(Method.PUT, "/users/459");
+        Assert.assertEquals(response.getStatusCode(), SUCCESS_200, response.body().asString());
 
         JsonPath path = response.jsonPath();
-        String result = path.get("updatedAt");
-        Assert.assertFalse(result.isEmpty());
+        String result = path.get("name");
+        Assert.assertTrue(result.contains("Mishanja"));
     }
 
     @Description("Delete user information")
     @Test
-    public void deleteUser(){
-        RequestSpecification request = RestAssured.given();
-        request.baseUri(apiAddress);
+    public void deleteUser() {
+        RequestSpecification request = getRequestSpecification();
 
-        Response response = request.request(Method.DELETE,"api/users/2");
-        Assert.assertEquals(response.getStatusCode(), SUCCESS_204);
+        Response response = request.request(Method.DELETE, "/users/2");
+        Assert.assertEquals(response.getStatusCode(), SUCCESS_204, response.body().asString());
     }
 
     @Description("Registration user account")
     @Test
-    public void registrationUser(){
-        RequestSpecification request = RestAssured.given();
-        request.baseUri(apiAddress);
+    public void registrationUser() {
+        RequestSpecification request = getRequestSpecification();
 
         JSONObject jsonBody = new JSONObject();
-        jsonBody.put("email","eve.holt@reqres.in");
-        jsonBody.put("password","pistol");
+        jsonBody.put("email", "eve.holt@reqres.in");
+        jsonBody.put("password", "pistol");
 
-        request.header(new Header("Content-Type","application/json"));
+        request.header(new Header("Content-Type", "application/json"));
         request.body(jsonBody.toString());
 
-        Response response = request.request(Method.POST,"api/register");
-        Assert.assertEquals(response.getStatusCode(), SUCCESS_CODE, response.prettyPrint());
+        Response response = request.request(Method.POST, "/register");
+        Assert.assertEquals(response.getStatusCode(), SUCCESS_200, response.body().asString());
 
-        JsonPath path = response.jsonPath();
-        System.out.println("id" + path.get("id"));
-        System.out.println("token" + path.get("token"));
     }
 
-
-
-
-
-
-
-
-
+    @Description("Unsuccessful registration check")
     @Test
-    public void doRegister() {
-        apiAddress += "doregister";
-
-        JSONObject requestBody = new JSONObject();
-        requestBody.put("name", "LukLukOsPOs9");
-        requestBody.put("email", "LukLukOsPOs9@ru.mail");
-        requestBody.put("password", "LukLukOsPOs9");
-
+    public void unsuccessfulRegistrationCheck() {
         RequestSpecification request = getRequestSpecification();
-        request.body(requestBody.toString());
+        request.header(new Header("Content-Type", "application/json"));
 
-        Response response = request.post(apiAddress);
-        int status = response.statusCode();
-        Assert.assertEquals(status, SUCCESS_CODE);
-        System.out.println("Actual status code is  " + status);
+
+        JSONObject objectBody = new JSONObject();
+        objectBody.put("email", "sydney@fife");
+        request.body(objectBody.toString());
+
+        Response response = request.request(Method.POST, "/register");
+        Assert.assertEquals(response.getStatusCode(), FAIL_400, response.asString());
     }
 
+    @Description("Login test")
     @Test
-    public void getUserFull() {
-        apiAddress += "getuserfull?email=oly123@gmail.com";
-        Response response;
-        RequestSpecification request = getRequestSpecification();
-        response = request.get(apiAddress);
-        Assert.assertEquals(response.getStatusCode(), SUCCESS_CODE);
+    public void loginCheck() {
+        RequestSpecification specification = getRequestSpecification();
+        specification.header(new Header("Content-Type", "application/json"));
 
-        JSONObject result = new JSONObject(response.body().asString());
-        Assert.assertEquals(result.getString("name"), "LukLukOs");
+        JSONObject jsonBody = new JSONObject();
+        jsonBody.put("email", "eve.holt@reqres.in");
+        jsonBody.put("password", "cityslicka");
 
+        specification.body(jsonBody.toString());
 
+        Response response = specification.request(Method.POST, "/login");
+        Assert.assertTrue(response.getStatusCode() == SUCCESS_200, "Login failed " + response.body().asString());
+        Assert.assertTrue(response.jsonPath().get("token").equals("QpwL5tke4Pnpja7X4"), response.jsonPath().get("token"));
+    }
+
+    @Description("Failed login test")
+    @Test
+    public void failedLoginCheck() {
+        RequestSpecification specification = getRequestSpecification();
+        specification.header(new Header("Content-Type", "application/json"));
+
+        JSONObject jsonBody = new JSONObject();
+        jsonBody.put("email", "eve.holt@reqres.in");
+
+        specification.body(jsonBody.toString());
+
+        Response response = specification.request(Method.POST, "/login");
+        Assert.assertTrue(response.getStatusCode() == FAIL_400, "Login failed " + response.body().asString());
+    }
+
+    @Description("Delay 3 seconds check ")
+    @Test
+    public void checkDelayResults() {
+        RequestSpecification specification = getRequestSpecification();
+
+        Response response = specification.queryParam("delay", "3").request(Method.GET, "/users");
+        Assert.assertEquals(response.getStatusCode(), SUCCESS_200);
+
+        int total_pages = response.jsonPath().get("total_pages");
+
+        Assert.assertTrue(total_pages == 2, response.body().asString());
+        Assert.assertTrue(response.time() > 3000, String.valueOf(response.time()));
     }
 
     private RequestSpecification getRequestSpecification() {
-        RequestSpecification request = RestAssured.given();
-        request.header("Content-Type", "application/json");
-//        request.auth().basic(login,password);
-        return request;
+        RequestSpecification specification = RestAssured.given();
+        specification.baseUri(apiAddress);
+        return specification;
     }
-
-//    @Test
-//    public void deleteUser() {
-//        apiAddress += "deleteuser";
-//        RestAssured.baseURI = apiAddress;
-//
-//        RequestSpecification request = getRequestSpecification();
-//
-//        JSONObject jsonObject = new JSONObject();
-//        jsonObject.put("email", "luklukospos9@ru.mail");
-//
-//        request.body(jsonObject.toString());
-//        Response response = request.request(Method.DELETE);
-//
-//        int statusCode = response.getStatusCode();
-//        Assert.assertTrue(statusCode == 200);
-//
-//
-//    }
-
-
 }
